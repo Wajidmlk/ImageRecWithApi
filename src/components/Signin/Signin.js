a928a7dbc5f66c69fa1ce85356a8d72bf92e495c
wajidmlk <wajidmlk123@gmail.com>
2021-03-13 01:45:55 +0500

    till 130321

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -42,14 +42,14 @@ class Signin extends React.Component{
 				    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
 				      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
 				      <div className="mt3">
-				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email (USE: malik@gmail.com)</label>
 				        <input 
 				        className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
 				        onChange={this.onEmailChange}
 				        type="email" name="email-address"  id="email-address" />
 				      </div>
 				      <div className="mv3">
-				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+				        <label className="db fw6 lh-copy f6" htmlFor="password">Password (USE: 123)</label>
 				        <input 
 				        className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        onChange={this.onPasswordChange}
